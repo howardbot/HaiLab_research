03e525bd1251718a7084146f9cf520d487eef1ab
howardbot <qliu385@wisc.edu>
2025-04-17 02:20:42 -0500

    Add centrality measurement

--- neuron_connectivity.py
+++ neuron_connectivity.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import networkx as nx
+
+
 from src.Loader import load_mat_session
 from src.Features import extract_firing_rate
 
@@ -11,6 +13,7 @@ data_dir = "./data"
 output_dir = "./Connectivity"
 os.makedirs(output_dir, exist_ok=True)
 
+#define data files
 mat_files = [f for f in os.listdir(data_dir) if f.endswith(".mat")]
 
 for fname in mat_files:
@@ -26,7 +29,7 @@ for fname in mat_files:
     X = X[:, :30] if X.shape[1] > 30 else X
 
     # Matrix calculation
-    corr_matrix = np.corrcoef(X.T)
+    corr_matrix = np.corrcoef(X.T) #Transpose of matrix
 
     # Heatmap
     plt.figure(figsize=(10, 8))
@@ -40,9 +43,23 @@ for fname in mat_files:
 
     # Construct the connectivity graph
     threshold = 0.5
+    # construct adjacency matrix
     adj = (np.abs(corr_matrix) > threshold).astype(int)
+    # don't connect itself
     np.fill_diagonal(adj, 0)
+    # Build an undirected graph
     G = nx.from_numpy_array(adj)
+
+    # centrality measures
+    degree_dict = dict(G.degree())
+    degree_centrality = nx.degree_centrality(G)
+    betweenness = nx.betweenness_centrality(G)
+    closeness = nx.closeness_centrality(G)
+
+    with open(os.path.join(output_dir,f"centraility_{fname}.txt"),'w') as f:
+        f.write("Neuron\tDegree\tDegCenter\tBetweeness\tCloseness\n")
+        for n in G.nodes():
+            f.write(f"{n}\t{degree_dict[n]}\t{degree_centrality[n]:.4f}\t{betweenness[n]:.4f}\t{closeness[n]:.4f}\n")
 
     plt.figure(figsize=(8, 8))
     nx.draw_networkx(G, node_color='skyblue', with_labels=True)
@@ -51,4 +68,4 @@ for fname in mat_files:
     plt.savefig(os.path.join(output_dir, f"graph_corr_{fname}.png"))
     plt.close()
 
-    print(f"✅ Done: heatmap + graph saved for {fname}")
+    print(f"Done: heatmap + graph saved for {fname}")
